7c8c52795f2d5626c0f1aaf9b7a1f726c22f7e3e
Scythe01 <113822220+Scythe01@users.noreply.github.com>
2022-09-18 15:27:19 +0800

    Add files via upload

---
+++ Tic_Tac_Toe_Game.py
@@ -0,0 +1,5 @@
+#Tic Tac Toe Game
+import pygame
+pygame.init(), pygame.display.set_caption('Tic Tac Toe')
+SIZE, Board = pygame.display.set_mode((500, 500)), [None, None, None, None, None, None, None, None, None]
+#Putting X and O
